b55e8fcd89d416e616c4398c21099d60c633d14c
sourav215 <sourav10010080@gmail.com>
2022-12-18 16:25:42 +0530

    Modified Stopwatch

--- BasicStopWatch/basicstopwatch/src/Components/Stopwatch.jsx
+++ BasicStopWatch/basicstopwatch/src/Components/Stopwatch.jsx
@@ -1,8 +1,33 @@
+import { useState, useEffect } from "react";
 function Stopwatch() {
-    return (
-        <div>
-            <h1>Timer</h1>
-        </div>
-    );
+  const [count, setCount] = useState(10);
+  let countId;
+  const startInterval = () => {
+    countId = setInterval(() => {
+      setCount((prev) => {
+        if (prev === 0) {
+          clearInterval(countId);
+          return prev;
+        } else {
+          return prev - 1;
+        }
+      });
+    },1000);
+  };
+
+  useEffect(() => {
+    startInterval();
+    return () => clearInterval(countId);
+  }, []);
+
+  return (
+    <div>
+      <h1>Timer</h1>
+      <h1>{count}</h1>
+      <button>Start</button>
+      <button>Pause</button>
+      <button onClick={startInterval}>Reset</button>
+    </div>
+  );
 }
 export default Stopwatch;
