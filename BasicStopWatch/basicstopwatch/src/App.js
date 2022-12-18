d346f86aaa2d2aec035583f6133f840d0e082a69
sourav215 <sourav10010080@gmail.com>
2022-12-18 16:03:00 +0530

    Modified Stopwatch

--- BasicStopWatch/basicstopwatch/src/App.js
+++ BasicStopWatch/basicstopwatch/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Stopwatch from './Components/Stopwatch';
 
 function App() {
   return (
     <div className="App">
-      
+      <Stopwatch/>
     </div>
   );
 }
---
+++ BasicStopWatch/basicstopwatch/src/Components/Stopwatch.jsx
@@ -0,0 +1,8 @@
+function Stopwatch() {
+    return (
+        <div>
+            <h1>Timer</h1>
+        </div>
+    );
+}
+export default Stopwatch;
