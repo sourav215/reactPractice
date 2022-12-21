9979a9330ac8fe1565685170b09c7cc365471706
sourav215 <sourav10010080@gmail.com>
2022-12-21 16:02:17 +0530

    Modified

--- ContextApi/contextapi/src/App.js
+++ ContextApi/contextapi/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import A from "./Components/NestedChild/A";
 
 function App() {
   return (
     <div className="App">
-     
+      <A />
     </div>
   );
 }
--- ContextApi/contextapi/src/Components/NestedChild/A.jsx
+++ ContextApi/contextapi/src/Components/NestedChild/A.jsx
@@ -1,3 +1,8 @@
 function A() {
-    
+    return (
+        <div style={{width: "600px", margin:"50px auto", border: "1px solid teal"}}>
+            <h1>A Component</h1>
+        </div>
+    );
 }
+export default A;
