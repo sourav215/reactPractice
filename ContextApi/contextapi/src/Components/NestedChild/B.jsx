f962319c4f48c1f81ee144fe10d196b59eb3c75e
sourav215 <sourav10010080@gmail.com>
2022-12-21 16:06:29 +0530

    Modified

--- ContextApi/contextapi/src/Components/NestedChild/B.jsx
+++ ContextApi/contextapi/src/Components/NestedChild/B.jsx
@@ -1,18 +1,15 @@
-import B from "./B";
-
-function A() {
+function B() {
   return (
     <div
       style={{
-        width: "800px",
+        width: "600px",
         padding: "20px",
         margin: "50px auto",
         border: "1px solid teal",
       }}
     >
-      <h1>A Component</h1>
-      <B />
+      <h1>B Component</h1>
     </div>
   );
 }
-export default A;
+export default B;
