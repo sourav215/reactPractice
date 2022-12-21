03f3eb680f834a66a7314ca1e9bf9ff7b4e6e225
sourav215 <sourav10010080@gmail.com>
2022-12-21 18:50:46 +0530

    Modified

---
+++ ContextApi/contextapi/src/Components/RegUser/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h2>Body</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. At quis doloribus libero. Voluptates veniam, libero ipsum ex corporis nulla assumenda optio hic distinctio omnis explicabo aperiam doloribus perferendis in id necessitatibus magnam voluptatem, neque consequuntur, et cumque nostrum. Corporis doloremque doloribus commodi accusamus sequi aut adipisci, sit quisquam unde minima!</p>
+        </div>
+    );
+}
+export default Home;
