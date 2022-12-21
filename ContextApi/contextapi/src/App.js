ce2c1d8787f9f266076d2a371af19dd20f0f2851
sourav215 <sourav10010080@gmail.com>
2022-12-21 18:44:35 +0530

    Modified

--- ContextApi/contextapi/src/App.js
+++ ContextApi/contextapi/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import A from "./Components/NestedChild/A";
+import Login from "./Components/RegUser/Login";
 
 function App() {
   return (
     <div className="App">
-      <A />
+      {/* <A /> */}
+      <Login/>
     </div>
   );
 }
--- ContextApi/contextapi/src/Components/RegUser/Login.jsx
+++ ContextApi/contextapi/src/Components/RegUser/Login.jsx
@@ -1,23 +1,52 @@
+import { useState } from "react";
+
 function Login() {
+  const [inputData, setInputData] = useState({
+    name: "",
+    password: "",
+  });
+
+  const handleValuedInput = (e) => {
+    setInputData ({...inputData, [e.target.name]: e.target.value})
+  };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputData);
+
+  }
+
   const loginStyle = {
     width: "400px",
     padding: "20px",
     margin: "50px auto",
-    textAlign: center,
+    textAlign: "center",
     boxShadow: "0 0 10px black",
   };
+
   return (
     <div style={loginStyle}>
       <div>
         <h2>Login Form</h2>
-        <form action="">
-            <input type="text" placeholder="Username.." />
-            <br />
-            <input type="text" placeholder="Password.." />
-            <br />
-            <input type="submit" value={"Submit"} />
+        <form action="" onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            placeholder="Username.."
+            name="name"
+            onChange={handleValuedInput}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Password.."
+            name="password"
+            onChange={handleValuedInput}
+          />
+          <br />
+          <input type="submit" value={"Submit"} />
         </form>
       </div>
     </div>
   );
 }
+
+export default Login;
