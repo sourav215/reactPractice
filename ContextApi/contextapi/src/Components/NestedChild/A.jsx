f940386646f6c63664326934de46e5d3338ed43b
sourav215 <sourav10010080@gmail.com>
2022-12-21 16:04:11 +0530

    Modified

--- ContextApi/contextapi/src/Components/NestedChild/A.jsx
+++ ContextApi/contextapi/src/Components/NestedChild/A.jsx
@@ -1,8 +1,15 @@
 function A() {
-    return (
-        <div style={{width: "600px", margin:"50px auto", border: "1px solid teal"}}>
-            <h1>A Component</h1>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        width: "800px",
+        padding: "20px",
+        margin: "50px auto",
+        border: "1px solid teal",
+      }}
+    >
+      <h1>A Component</h1>
+    </div>
+  );
 }
 export default A;
