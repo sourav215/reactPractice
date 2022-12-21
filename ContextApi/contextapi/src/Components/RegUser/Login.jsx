ebb51e127a2ab641dbc6a9ab568c50b840c62bac
sourav215 <sourav10010080@gmail.com>
2022-12-21 18:35:43 +0530

    Modified

--- ContextApi/contextapi/src/Components/RegUser/Login.jsx
+++ ContextApi/contextapi/src/Components/RegUser/Login.jsx
@@ -1,17 +1,23 @@
-
 function Login() {
-
-
-    const loginStyle = {
-        width: "400px",
-        padding: "20px",
-        margin: "50px auto",
-        textAlign: center,
-        boxShadow: "0 0 10px black"
-    }
-    return (
-        <div style={loginStyle}>
-
-        </div>
-    );
+  const loginStyle = {
+    width: "400px",
+    padding: "20px",
+    margin: "50px auto",
+    textAlign: center,
+    boxShadow: "0 0 10px black",
+  };
+  return (
+    <div style={loginStyle}>
+      <div>
+        <h2>Login Form</h2>
+        <form action="">
+            <input type="text" placeholder="Username.." />
+            <br />
+            <input type="text" placeholder="Password.." />
+            <br />
+            <input type="submit" value={"Submit"} />
+        </form>
+      </div>
+    </div>
+  );
 }
