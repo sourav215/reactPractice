fdc84eeafb294cbc13f8b92e3631ea81c455bbcf
sourav215 <sourav10010080@gmail.com>
2022-12-21 19:26:01 +0530

    Modified

--- ContextApi/contextapi/src/App.js
+++ ContextApi/contextapi/src/App.js
@@ -1,15 +1,21 @@
-
 import "./App.css";
 import A from "./Components/NestedChild/A";
 import Auth from "./Components/RegUser/Auth";
 import Navbar from "./Components/RegUser/Navbar";
-
+import { useState } from "react";
+import loginContext from "./Components/RegUser/LoginContext";
 function App() {
+  const [userDetails, setUserDetails] = useState({
+    user: "",
+    loginStatus: "",
+  });
   return (
     <div className="App">
       {/* <A /> */}
-      <Navbar />
-      <Auth />
+      <loginContext.Provider value={userDetails}>
+        <Navbar />
+        <Auth />
+      </loginContext.Provider>
     </div>
   );
 }
