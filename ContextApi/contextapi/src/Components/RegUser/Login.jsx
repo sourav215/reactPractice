f833b160e2a61b9f44ac54f3e9dcd1134b8f8d6f
sourav215 <sourav10010080@gmail.com>
2022-12-21 18:33:09 +0530

    Modified

---
+++ ContextApi/contextapi/src/Components/RegUser/Login.jsx
@@ -0,0 +1,17 @@
+
+function Login() {
+
+
+    const loginStyle = {
+        width: "400px",
+        padding: "20px",
+        margin: "50px auto",
+        textAlign: center,
+        boxShadow: "0 0 10px black"
+    }
+    return (
+        <div style={loginStyle}>
+
+        </div>
+    );
+}
