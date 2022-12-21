364060a0286bfd623e8f757189365d11e8e00449
sourav215 <sourav10010080@gmail.com>
2022-12-21 19:11:37 +0530

    Modified

--- ContextApi/contextapi/src/Components/RegUser/Navbar.jsx
+++ ContextApi/contextapi/src/Components/RegUser/Navbar.jsx
@@ -1,6 +1,14 @@
 function Navbar() {
+  const navStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    backgroundColor: "#2C3E50",
+    color: "#fff",
+    padding: "20px",
+    
+  };
   return (
-    <div>
+    <div style={navStyle}>
       <div>
         <span style={{ fontSize: "25px" }}>React</span>
       </div>
@@ -8,7 +16,7 @@ function Navbar() {
         <a href="#">Home</a>
         <a href="#">Profile</a>
         <a href="#">Product</a>
-        <span style={{ marginRight: "30px", color: "green", fontSize: "20px" }}>
+        <span style={{ marginRight: "30px", color: "#fff", fontSize: "20px" }}>
           Guest
         </span>
         <button
@@ -16,7 +24,8 @@ function Navbar() {
             marginRight: "50px",
             backgroundColor: "#E74C3C",
             color: "#fff",
-            fontSize: "20px",
+            fontSize: "16px",
+            letterSpacing: "1px"
           }}
         >
           Logout
