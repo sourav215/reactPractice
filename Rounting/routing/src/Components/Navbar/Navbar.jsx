3ee72e31906ccdd251a261a73ff1f92078351def
sourav215 <sourav10010080@gmail.com>
2022-12-22 15:07:57 +0530

    Modified

--- Rounting/routing/src/Components/Navbar/Navbar.jsx
+++ Rounting/routing/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,17 @@
 import nv from "./Navbar.module.css";
+import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className={nv.navbar}>
       <div>
         <span>React</span>
       </div>
-      <div >
-        <a href="/">Home</a>
-        <a href="/profile">Profile</a>
-        <a href="/todo">Todo</a>
-        <a href="/products">Products</a>
-        <a href="/counter">Counter</a>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/todo">Todo</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/counter">Counter</Link>
       </div>
     </div>
   );
