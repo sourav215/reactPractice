fd52f2cd6131f792d0df3e44a70376e5ede87c97
sourav215 <sourav10010080@gmail.com>
2022-12-22 13:38:28 +0530

    Modified

--- Rounting/routing/src/Components/Navbar/Navbar.jsx
+++ Rounting/routing/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,18 @@
- function Navbar(){
-    return (
-        <div>
-            
-        </div>
-    );
- }
+import nv from "./Navbar.module.css";
+function Navbar() {
+  return (
+    <div className={nv.navbar}>
+      <div>
+        <span>React</span>
+      </div>
+      <div>
+        <a href="">Home</a>
+        <a href="">Profile</a>
+        <a href="">Todo</a>
+        <a href="">Products</a>
+        <a href="">Counter</a>
+      </div>
+    </div>
+  );
+}
+export default Navbar;
