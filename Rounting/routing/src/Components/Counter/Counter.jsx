dc3ca678a562a1077e7042b1d430c441ba7d2619
sourav215 <sourav10010080@gmail.com>
2022-12-23 18:24:37 +0530

    Modified

--- Rounting/routing/src/Components/Counter/Counter.jsx
+++ Rounting/routing/src/Components/Counter/Counter.jsx
@@ -1,8 +1,11 @@
-function Counter () {
-return (
+import { useSearchParams } from "react-router-dom";
+function Counter() {
+  const [searchparam, setSearchparam] = useSearchParams();
+  console.log(searchparam.get("gender"));
+  return (
     <div>
-        <h1>Counter Component</h1>
+      <h1>Counter Component</h1>
     </div>
-);
+  );
 }
 export default Counter;
