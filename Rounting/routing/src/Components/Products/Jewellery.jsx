192a0967d91198d172d593a5c680cee8f23d91a1
sourav215 <sourav10010080@gmail.com>
2022-12-23 17:09:15 +0530

    Modified

--- Rounting/routing/src/Components/Products/Jewellery.jsx
+++ Rounting/routing/src/Components/Products/Jewellery.jsx
@@ -1,8 +1,53 @@
+import { useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+
 function Jewellery() {
-    return (
-        <div>
-            <h1>Jewellery Component</h1>
-        </div>
+  const [jdata, setJData] = useState([]);
+
+  const getData = async () => {
+    let res = await fetch(
+      `https://fakestoreapi.com/products/category/jewelery`
     );
+    let data = await res.json();
+    setJData(data);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div>
+      <h1>Jewellery Component</h1>
+      <div>
+        {jdata.length > 0 ? (
+          <ul
+            style={{
+              listStyleType: "none",
+              textAlign: "left",
+              fontSize: "20px",
+            }}
+          >
+            {jdata.map((ele, i) => {
+              return (
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    color: "teal",
+                    display: "block",
+                    marginBottom: "20px",
+                  }}
+                >
+                  <li key={i}>{ele.title}</li>
+                </Link>
+              );
+            })}
+          </ul>
+        ) : (
+          <div>
+            <h2>....loading..</h2>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 export default Jewellery;
