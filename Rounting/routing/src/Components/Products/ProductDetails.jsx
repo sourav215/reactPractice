2bc32ba875e1ab9d105d175a9903c4e8b89b02c0
sourav215 <sourav10010080@gmail.com>
2022-12-23 18:06:53 +0530

    Modified

--- Rounting/routing/src/Components/Products/ProductDetails.jsx
+++ Rounting/routing/src/Components/Products/ProductDetails.jsx
@@ -1,9 +1,41 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+function ProductDetails() {
+  const [prod, setProd] = useState({});
 
-function ProductDetails(){
-    return (
+  const { id } = useParams();
+
+  const getSigleProdData = async () => {
+    try {
+      let res = await fetch(`https://fakestoreapi.com/products/${id}`);
+      let resData = await res.json();
+      setProd(resData);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  useEffect(() => {
+    getSigleProdData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Product Details</h1>
+      {Object.keys(prod).length > 0 ? (
         <div>
-            <h1>Product Details</h1>
+          <img src={prod.image} alt="" />
+          <h2>{prod.title}</h2>
+          <h4>$ {prod.price}</h4>
+          <h4>{prod.category.toUpperCase()}</h4>
         </div>
-    );
+      ) : (
+        <div>
+          {" "}
+          <h1>...No Data..</h1>
+        </div>
+      )}
+    </div>
+  );
 }
 export default ProductDetails;
