2c0bf23083b54da53cb5d19b614a478b66398e29
sourav215 <sourav10010080@gmail.com>
2022-12-29 15:11:19 +0530

    Modified

--- ReduxPattern/reduxpattern/src/Components/Redux/TaskReducer.jsx
+++ ReduxPattern/reduxpattern/src/Components/Redux/TaskReducer.jsx
@@ -1,5 +1,8 @@
 const initialState = [];
 const taskReducer = (state = initialState, action) => {
+  if (action.type === "ADD") {
+    state = [...state, action.payload];
+  }
   return state;
 };
 export default taskReducer;
