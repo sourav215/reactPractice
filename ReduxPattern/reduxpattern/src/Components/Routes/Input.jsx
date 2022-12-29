6ccf66e3e7b9ab1a471916b5988493560edf735e
sourav215 <sourav10010080@gmail.com>
2022-12-29 15:14:30 +0530

    Modified

--- ReduxPattern/reduxpattern/src/Components/Routes/Input.jsx
+++ ReduxPattern/reduxpattern/src/Components/Routes/Input.jsx
@@ -1,9 +1,22 @@
+import taskAction from "../Redux/Action";
+import { useState } from "react";
+
 function Input() {
+  const [inputState, setInputState] = useState("");
+  const handleSubmit = () => {
+    console.log(inputState);
+  };
   return (
     <div style={{ padding: "100px" }}>
       <h2>Input Component</h2>
-      <input type="text" placeholder="Enter task here ..." />
-      <button>Submit</button>
+      <input
+        type="text"
+        placeholder="Enter task here ..."
+        onChange={(e) => {
+          setInputState(e.target.value);
+        }}
+      />
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
