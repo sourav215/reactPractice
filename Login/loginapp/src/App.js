be4a3fa6ea16ebdad35a6ac6aecc0a244a2f72ab
sourav215 <sourav10010080@gmail.com>
2023-01-05 23:45:39 +0530

    Modified

--- Login/loginapp/src/App.js
+++ Login/loginapp/src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "@chakra-ui/react";
 import Login from "./Components/Login/Login";
+import AllRoutes from "./Components/Routes/AllRoutes";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <AllRoutes />
     </div>
   );
 }
--- Login/loginapp/src/Components/Routes/AllRoutes.jsx
+++ Login/loginapp/src/Components/Routes/AllRoutes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
+import Login from "../Login/Login";
 import Home from "./Home";
 function AllRoutes() {
-    return(
-        <div>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-        </Routes>
-        </div>
-    )
+  return (
+    <div>        
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 export default AllRoutes;
