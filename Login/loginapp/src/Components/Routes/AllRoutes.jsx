a18f4eeb3811a2f5edcd40dbd32e6d90c22d2c41
sourav215 <sourav10010080@gmail.com>
2023-01-06 20:18:35 +0530

    Modified

--- Login/loginapp/src/Components/Routes/AllRoutes.jsx
+++ Login/loginapp/src/Components/Routes/AllRoutes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "../Login/Login";
 import Home from "./Home";
+import Policy from "./Policy";
 function AllRoutes() {
   return (
     <div>        
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/policy" element={<Policy/>}/>
       </Routes>
     </div>
   );
