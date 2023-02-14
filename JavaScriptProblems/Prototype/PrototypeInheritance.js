ae5f8cbae60cea6a7a66db2abcc39f69e1776099
sourav215 <sourav10010080@gmail.com>
2023-02-15 02:09:41 +0530

    Added Custom Map Filter Reduce

--- JavaScriptProblems/Prototype/PrototypeInheritance.js
+++ JavaScriptProblems/Prototype/PrototypeInheritance.js
@@ -1,25 +1,27 @@
 // using Object.create()
 function Automobile(name, model, color) {
-    this.companyName = name;
-    this.model = model;
-    this.color = color;
-  }
-  let nexon = new Automobile("Tata", "Nexon", "red");
-  console.log(nexon);
-  
-  // it creates an empty object
-  let obj = Object.create(nexon);
-  console.log(obj);
-  
-  function Car(fuelType) {
-    this.fuelType = fuelType;
-  }
-  
-  // Car.prototype = Automobile;
-  
-  Car.prototype = nexon;
-  let obj2 = new Car("Petrol");
-  
-  console.log(obj2);
-  console.log(obj2.model);
-  
+  this.companyName = name;
+  this.model = model;
+  this.color = color;
+}
+let nexon = new Automobile("Tata", "EV", "red");
+console.log(nexon);
+
+
+// using Object.create
+let obj = Object.create(nexon);
+console.log(obj);
+console.log(obj.model);
+
+
+// using Prototype
+function Car(fuelType) {
+  this.fuelType = fuelType;
+}
+
+Car.prototype = nexon;
+let obj2 = new Car("Petrol");
+// obj2.__proto__ = nexon;
+
+console.log(obj2);
+console.log(obj2.model);
