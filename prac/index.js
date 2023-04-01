a2272e63c9e6e2ffe5f27ba396553888aaeeed17
sourav215 <sourav10010080@gmail.com>
2023-04-01 21:19:03 +0530

    Modified

--- prac/index.js
+++ prac/index.js
@@ -3,9 +3,29 @@ async function getData() {
     let res = await fetch("https://fakestoreapi.com/products");
     let data = await res.json();
     console.log(data);
+    displayData(data);
   } catch (err) {
     console.log(err);
   }
+}
+
+function displayData(allItems) {
+  let container = document.querySelector(".container");
+  container.innerHTML= "";
+
+  allItems.forEach((ele, i) => {
+    let div = document.createElement("div");
+    let title = document.createElement("h2");
+    title.textContent = ele.title;
+    let img = document.createElement("img");
+    img.src = ele.image;
+    let description = document.createElement("p");
+    description.textContent  = ele.description;
+    div.append(title, img, description);
+    container.append(div);
+
+
+  });
 }
 
 document.querySelector("#button").addEventListener("click", getData);
