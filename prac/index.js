eca45c06568fee986f2b784a4c2a7c52eba0d648
sourav215 <sourav10010080@gmail.com>
2023-04-01 20:11:56 +0530

    Modified

--- prac/index.js
+++ prac/index.js
@@ -1,1 +1,11 @@
+async function getData() {
+  try {
+    let res = await fetch("https://fakestoreapi.com/products");
+    let data = await res.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
+document.querySelector("#button").addEventListener("click", getData);
