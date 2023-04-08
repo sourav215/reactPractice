dfaf8affcd6baac56e094f012c762373ea6c3e31
sourav215 <sourav10010080@gmail.com>
2023-04-08 23:40:59 +0530

    Modified

--- prac/index.js
+++ prac/index.js
@@ -9,23 +9,3 @@ async function getData() {
   }
 }
 
-function displayData(allItems) {
-  let container = document.querySelector(".container");
-  container.innerHTML= "";
-
-  allItems.forEach((ele, i) => {
-    let div = document.createElement("div");
-    let title = document.createElement("h2");
-    title.textContent = ele.title;
-    let img = document.createElement("img");
-    img.src = ele.image;
-    let description = document.createElement("p");
-    description.textContent  = ele.description;
-    div.append(title, img, description);
-    container.append(div);
-
-
-  });
-}
-
-document.querySelector("#button").addEventListener("click", getData);
