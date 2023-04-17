d301f7540a84f6e2f6ccbab8eb7d0a7c5623f555
sourav215 <sourav10010080@gmail.com>
2023-04-17 19:58:48 +0530

    Modified

--- prac/index.js
+++ prac/index.js
@@ -1,3 +1,14 @@
-console.dir(this)
+function showModal() {
+  document.querySelector(".overlay").classList.add("showoverlay");
+  document.querySelector(".login-form").classList.add("showlogin-form");
+}
 
+function hideModal() {
+  document.querySelector(".overlay").classList.remove("showoverlay");
+  document.querySelector(".login-form").classList.remove("showlogin-form");
+}
 
+document.querySelector(".btn-login").addEventListener("click", showModal);
+
+document.querySelector(".hideform").addEventListener("click", hideModal);
+document.querySelector(".overlay").addEventListener("click", hideModal);
